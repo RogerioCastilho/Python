93bba77dad68ea5fdc6ad9862618806c137f6ed5
RogerioCastilho <rogeriochequer@yahoo.com.br>
2020-09-04 01:39:24 -0300

    Exclude conditional variable
    Excluded conditional variable for while loop

--- Jo_Ken_Po.py
+++ Jo_Ken_Po.py
@@ -9,26 +9,25 @@ from random import randint
 import time
 
 choose = {0:"Paper",1:"Rock",2:"Scissors"}
-condition = True
 computer = 0
 
 print('*'*20)
 print('JO KEN PO')
 print('*'*20)
 
-
-while condition == True:
+while True:
     for c in choose:
         print(f"{c} - {choose[c]}")
     player = input('Make your choice:')
 
     if player == '0' or player == '1' or player == '2':
-        condition = False
+        break
     else:
         print('Wrong answer, please try again!')
-              
+             
 computer = randint(0,2)
 player = int(player)
+
 print(end='\n')
 print('JO')
 time.sleep(0.5)
@@ -37,11 +36,13 @@ time.sleep(0.5)
 print('PO')
 time.sleep(1)
 print(end='\n')
+
 if (player == 0 and computer == 1) or (player == 1 and computer == 2) or (player == 2 and computer == 1):
        print('You won! DAMN IT!')
 elif player == computer:
        print('Draw')
 else:
        print('I won!!! LOOOSER!!!!')
-print(end='\n')      
+print(end='\n')
+      
 print(f"You played {choose[player]} and I played {choose[computer]}")
