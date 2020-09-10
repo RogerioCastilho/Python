cf9fd9c66e24c32fa379c78a210b610d1bd4e3c2
RogerioCastilho <rogeriochequer@yahoo.com.br>
2020-09-10 02:59:19 -0300

    Adding comments
    Adding comments

--- Jo_Ken_Po.py
+++ Jo_Ken_Po.py
@@ -8,24 +8,27 @@ Created on Tue Aug 25 00:32:09 2020
 from random import randint
 import time
 
-choose = {0:"Paper",1:"Rock",2:"Scissors"}
+choose = {0:"Paper",1:"Rock",2:"Scissors"} # Choices to play.
 computer = 0
-
+player = 0
 print('*'*20)
 print('JO KEN PO')
 print('*'*20)
 
+# It presents the options to the player and asks his input.
 while True:
-    for c in choose:
-        print(f"{c} - {choose[c]}")
-    player = input('Make your choice:')
-
-    if player == '0' or player == '1' or player == '2':
-        break
-    else:
+   for c in choose:
+       print(f"{c} - {choose[c]}")
+   player = input('Make your choice:')
+#Validate the input
+   if player == '0' or player == '1' or player == '2':
+       break
+   else:
         print('Wrong answer, please try again!')
+
              
-computer = randint(0,2)
+computer = randint(0,2) #Computer make his choice.
+
 player = int(player)
 
 print(end='\n')
@@ -37,12 +40,14 @@ print('PO')
 time.sleep(1)
 print(end='\n')
 
-if (player == 0 and computer == 1) or (player == 1 and computer == 2) or (player == 2 and computer == 1):
+#Possible results.
+if (player == 0 and computer == 1) or (player == 1 and computer == 2) or (player == 2 and computer == 0):
        print('You won! DAMN IT!')
 elif player == computer:
        print('Draw')
 else:
        print('I won!!! LOOOSER!!!!')
 print(end='\n')
-      
+
+#Show chose options.      
 print(f"You played {choose[player]} and I played {choose[computer]}")
